8395d6e06adfe750d90da7e6c8135e256f461865
laschuet <laschuet@users.noreply.github.com>
2021-01-20 10:01:28 +0100

    Move kmeans clustering to extra function

--- examples/liqd.jl
+++ examples/liqd.jl
@@ -27,17 +27,50 @@ push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"""
 }
 """)
 
+const CONFIG = Dict("out_dir" => "out")
+
 function assignmentplot(assignments, x, y, xlabel, ylabel)
     t = @pgf Table({ meta = "cluster" }, x=x, y=y, cluster=assignments)
     p = @pgf Plot({ scatter, "only marks", scatter_src = "explicit", mark_size = "1pt" }, t)
     return @pgf Axis({ xlabel = xlabel, ylabel = ylabel }, p)
 end
 
+function clusterkmeans(data, ks, dist)
+    clusterings = []
+    Js = []
+    silhouette_coefficients = []
+
+    distances = pairwise(dist, data')
+    for k in ks
+        clustering = kmeans(data', k, distance=dist)
+        push!(clusterings, clustering)
+        push!(Js, clustering.totalcost)
+        push!(silhouette_coefficients, mean(silhouettes(clustering, distances)))
+    end
+
+    ## Assignment plots
+    for k in ks
+        plt = assignmentplot(assignments(clusterings[k - 1]), data[:, 1],data[:, 2], "longitude", "latitude")
+        pgfsave(CONFIG["out_dir"] * "/kmeans_assignments_k_" * (k < 10 ? "0$k" : "$k") * ".pdf", plt)
+    end
+
+    ## Elbow method plot
+    c = @pgf Coordinates(zip(ks, Js))
+    p = @pgf Plot({ color = "blue", mark = "x" }, c)
+    plt = @pgf Axis({ xlabel = raw"\(k\)", ylabel = raw"\(J\)" }, p)
+    pgfsave(CONFIG["out_dir"] * "/kmeans_elbow.pdf", plt)
+
+    ## Silhouette coefficient plot
+    c = @pgf Coordinates(zip(ks, silhouette_coefficients))
+    p = @pgf Plot({ color = "red", mark = "x" }, c)
+    plt = @pgf Axis({ xlabel = raw"\(k\)", ylabel = "silhouette coefficient" }, p)
+    pgfsave(CONFIG["out_dir"] * "/kmeans_silhouette.pdf", plt)
+end
+
 function main(dbpath, tablename)
     MEAN_EARTH_RADIUS = 6371
     earth_haversine = Haversine(MEAN_EARTH_RADIUS)
-    OUT_PATH = "out"
-    !isdir(OUT_PATH) && mkdir(OUT_PATH)
+    !isdir(CONFIG["out_dir"]) && mkdir(CONFIG["out_dir"])
 
     # Load data
     db = SQLite.DB(dbpath)
@@ -56,45 +89,7 @@ function main(dbpath, tablename)
     # display(data)
 
     # k-means
-    k_range = 2:2
-    distances = pairwise(earth_haversine, data')
-    clusterings = []
-    js = []
-    silhouette_coefficients = []
-    for k in k_range
-        clustering = kmeans(data', k, distance=earth_haversine)
-        push!(clusterings, clustering)
-
-        push!(js, clustering.totalcost)
-
-        mean_silhouette = mean(silhouettes(clustering, distances))
-        push!(silhouette_coefficients, mean_silhouette)
-    end
-
-    # Evaluate k-means
-    ## Assignment plots
-    for k in k_range
-        plt = assignmentplot(assignments(clusterings[k - 1]), longitude, latitude, "longitude", "latitude")
-        pgfsave("$OUT_PATH/kmeans_assignments_k_" * (k < 10 ? "0$k" : "$k") * ".pdf", plt)
-    end
-    ## Elbow method
-    plt = @pgf Axis({
-        xlabel = raw"\(k\)",
-        ylabel = raw"\(J\)",
-    }, Plot({
-        color = "blue",
-        mark = "x",
-    }, Coordinates(zip(k_range, js))))
-    pgfsave("$OUT_PATH/kmeans_elbow.pdf", plt)
-    ## Silhouette coefficient
-    plt = @pgf Axis({
-        xlabel = raw"\(k\)",
-        ylabel = "silhouette coefficient",
-    }, Plot({
-        color = "red",
-        mark = "x",
-    }, Coordinates(zip(k_range, silhouette_coefficients))))
-    pgfsave("$OUT_PATH/kmeans_silhouette.pdf", plt)
+    clusterkmeans(data, 2:12, earth_haversine)
 
     # DBSCAN
     distances = pairwise(earth_haversine, data')
@@ -109,7 +104,7 @@ function main(dbpath, tablename)
     }, Plot({
         color = "green"
     }, Coordinates(zip(1:length(avg_knn_distances), avg_knn_distances))))
-    pgfsave("$OUT_PATH/dbscan_eps.pdf", plt)
+    pgfsave(CONFIG["out_dir"] * "/dbscan_eps.pdf", plt)
 
     clusterings = []
     clustering = dbscan(distances, 0.7, k)
@@ -118,7 +113,7 @@ function main(dbpath, tablename)
     # Evaluate DBSCAN
     ## Assignment plots
     plt = assignmentplot(assignments(clusterings[1]), longitude, latitude, "longitude", "latitude")
-    pgfsave("$OUT_PATH/dbscan_assignments_eps_0.7_min_pts_5.pdf", plt)
+    pgfsave(CONFIG["out_dir"] * "/dbscan_assignments_eps_0.7_min_pts_5.pdf", plt)
 end
 
 main("~/datasets/participation/liqd_laermorte_melden.sqlite", "contribution")
