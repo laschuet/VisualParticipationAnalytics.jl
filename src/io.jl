57d4a534d5fdd91cc3a994e613bf94807294bc01
laschuet <laschuet@users.noreply.github.com>
2021-10-07 08:38:00 +0200

    Finish clustering object format

--- src/io.jl
+++ src/io.jl
@@ -14,12 +14,6 @@ function save(clustering::DbscanResult, filename::AbstractString)
     k = nclusters(clustering) + 1 # unassigned instances / noise
     ns = counts(clustering)
 
-    clusters = Vector{Dict}(undef, k)
-    clusters[1] = Dict("id" => 1, "n" => length(y) - sum(ns), "J" => 0.0)
-    for i = 2:k
-        clusters[i] = Dict("id" => i, "n"  => ns[i - 1] , "J" => 0.0)
-    end
-
     enumeratedy = enumerate(y)
     assigns = map(collect, enumeratedy)
 
@@ -31,12 +25,20 @@ function save(clustering::DbscanResult, filename::AbstractString)
         push!(invassigns[yi][2], i)
     end
 
+    clusters = Vector{Dict}(undef, k)
+    proto = length(invassigns[1][2]) == 0 ? 0 : invassigns[1][2][end]
+    clusters[1] = Dict("id" => 1, "n" => length(y) - sum(ns), "J" => 0.0, "proto" => proto)
+    for i = 2:k
+        proto = length(invassigns[i][2]) == 0 ? 0 : invassigns[i][2][end]
+        clusters[i] = Dict("id" => i, "n"  => ns[i - 1] , "J" => 0.0, "proto" => proto)
+    end
+
     result = Dict("clusters" => clusters, "assignments" => assigns, "invAssignments" => invassigns)
 
     open(filename, "w") do io
         JSON3.write(io, result)
     end
-    open("pretty_" * filename, "w") do io
+    open(filename * ".pretty", "w") do io
         JSON3.pretty(io, result)
     end
 end
